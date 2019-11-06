2da2c1400ad2a06100eae98db10bf440e9a6ea1f
Panlf <liangfeng_pan@163.com>
2019-11-06 18:15:01 +0800

    edit

--- operate_db.sh
+++ operate_db.sh
@@ -1,14 +1,25 @@
 #! /bin/bash
-database_names=`mysql -uroot -proot -h 127.0.0.1 -P3360 -e "show databases like '%_test';" | awk 'NR>1{print $1}'`
+
+USERNAME=root 
+PASSWORD=root
+PORT=3306
+IPADDRESS=172.0.0.1
+
+
+database_names=`mysql -u${USERNAME} -p${PASSWORD} -h ${IPADDRESS} -P${PORT} -e "show databases like '%_test';" | awk 'NR>1{print $1}'`
 for name in $database_names;
 do
-    #echo $name
-    table_names=`mysql -uroot -proot -h 127.0.0.1 -P3360 -e "select table_name from information_schema.tables where table_schema='$name' | awk 'NR>1{print $1}'`
+     #echo $name
+   table_names=`mysql -u${USERNAME} -p${PASSWORD} -h ${IPADDRESS} -P${PORT} -e "select table_name from information_schema.tables where table_schema='$name'" | awk 'NR>1{print $1}'`
     for table_name in $table_names;
-		do
-		#echo $table_name
-		table_count=`mysql -uroot -proot -h 127.0.0.1 -P3360 -e "select max(id) as count from $name.$table_name;" | tail -1`
-		echo "$name.$table_name=$table_count" >> /tmp/count.log
+    do
+	#echo $table_name
+	table_count=`mysql -u${USERNAME} -p${PASSWORD} -h ${IPADDRESS} -P${PORT} -e "select count(1) as count from $name.$table_name;" | tail -1`
+	if [ $table_count -eq 0 ];then
+		echo "$name.$table_name=$table_count" >> /tmp/table_count_zero.log
+	else	
+		echo "$name.$table_name=$table_count" >> /tmp/table_count.log
+	fi
     done
 done
 
